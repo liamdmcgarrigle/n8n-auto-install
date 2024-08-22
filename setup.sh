0609072325b5557e53e5528d8bfe66e059c85e2b
Liam McGarrigle <liam@mcgarrigle.co>
2024-08-21 21:18:48 -0400

    fixed issues

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 source n8n-auto-install/env/bin/activate
 
 # Install requirements
-n8n-auto-install/n8n_venv/bin/pip install -r n8n-auto-install/requirements.txt -q
+n8n-auto-install/env/bin/pip install -r n8n-auto-install/requirements.txt -q
 
 # Run the Python script
 python3 n8n-auto-install/main.py
